b3a7a9d8dcbac4e26e1a337ea2f430015d2d6349
AtulSangwan <atulsangwan152@gmail.com>
2023-06-27 00:25:14 +0530

    made changes

--- experiment/procedure.md
+++ experiment/procedure.md
@@ -1,1 +1,10 @@
-### Procedure
+## Procedure
+
+This experiment provides you with visual representations of various elements of the binomial expansion.
+<br>
+
+There is always a set of binomial expansion elements visible on screen whose visual representation can be chosen from.
+Use first text to enter the degree n of binomial coefficient with values between among 0,1,2 and 3. Click on next after entering the value. 
+Use the second text field to enter k which decides the element of the expansion for a given n, thus its values range from 0 to n.
+Click on generate to view the visual representation of the chosen element.
+This will display the visual representation of the binomial expansion of the chosen degree, that of the chosen element and will highlight the chosen element among the elements of binomial expanion on screen, in red.
--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,73 @@
 //Your JavaScript goes in here
+document.body.setAttribute("class","gridBody");
+let controlsDiv = document.getElementsByClassName('column is-2-desktop is-12-tablet is-12-mobile')[0];
+controlsDiv.style.width = 'auto';
+//controlsDiv.classList.add("widthAuto");
+
+let n;
+let k;
+document.getElementById('button2').style.visibility = 'hidden';
+function next(){
+
+    n = document.getElementById("n").value;
+    let copyN = n;
+    n = Number(n);
+    const isInteger = Number.isInteger(n);
+
+if(copyN && isInteger && n>=0 && n<4){
+const label = document.createElement("label");
+const node = document.createTextNode("enter k");
+label.appendChild(node);
+const input2 = document.createElement("input");
+input2.setAttribute("id","input2");
+input2.setAttribute("type","number");
+input2.setAttribute("max",n);
+input2.setAttribute("min","0");
+const element = document.getElementById("controls");
+element.innerHTML="";
+element.appendChild(label);
+element.appendChild(input2);
+
+document.getElementById('button1').style.visibility = 'hidden';
+document.getElementById('button2').style.visibility = 'visible';
+document.getElementById('n').disabled = true;   
+document.getElementById('errorId').innerHTML = "";         
+}
+else{
+    document.getElementById('errorId').innerHTML = "n should be an integer between 0 and 3";
+}}
+function Generate()
+{
+    const alreadySelected = document.querySelector('.selection');
+    if (alreadySelected) {
+        alreadySelected.classList.remove('selection');
+    }
+    let k = document.getElementById("input2").value;
+    let copyk = k;
+    k = Number(k);
+    const isIntegerk = Number.isInteger(n);
+    if(copyk && isIntegerk && k>=0 && k<=n){
+    console.log({ n, k })
+    let selection = document.getElementById(`n${n}${k}`);
+    selection.setAttribute("class","selection");
+    const image1 = document.getElementById('output');
+    image1.src=`images/${n}.jpg`;
+    const image2 = document.getElementById('output2');
+    image2.src=`images/${n}${k}.jpg`;
+    image1.setAttribute("class","image1");
+    image2.setAttribute("class","image2");
+    document.getElementById('errorId').innerHTML = "";
+    }
+    else{
+        document.getElementById('errorId').innerHTML = "k should be an integer between 0 and n"
+    }
+    // document.getElementById("output").appendChild(image);
+};
+function reload(){
+window.location.reload();
+}
+/*
+{
+    const image='<img src=images/sample.jpg></img>';
+    document.getElementById("output").insertAdjacentHTML("beforeend",image);
+};*/
