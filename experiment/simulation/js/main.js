3fabfc49598e813bd01e4c4bce283c1b1835df1e
AtulSangwan <atulsangwan152@gmail.com>
2024-01-26 11:40:05 +0530

    s

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -6,9 +6,14 @@ controlsDiv.style.width = 'auto';
 
 let n;
 let k;
+let n_arr=["the cuboid representing n=0 is just a point as the binomial expansion for n=0 is just 1.","the cuboid representing n=1 is a single line split in two parts with the length a+b","the cuboid representing n=2 is square divided into four parts with the length of a side being a+b","the cuboid representing n=3 is a cube divided into eight parts with the length of a side being a+b"]
+let k0=["k=0 for n=0 represents the constant 1 which is just a point"]
+let k1=["k=0 for n=1 represents a line with the length a from the binomial expansion term a","k=1 for n=1 represents a line with the length b from the binomial expansion term b"]
+let k2=["k=0 for n=2 represents a square with the length of a side equal to a from the binomial expansion term a^2","k=2 for n=2 represents a square with the length of a side equal to b from the binomial expansion term b^2","k=2 for n=2 represents a square with the length of a side equal to b from the binomial expansion term b^2"]
+let k3=["k=0 for n=3 represents a^3 from thr binomial expansion in the form of a cube with the length of its side equal to a","k=1 for n=3 represents 3a^2b from thr binomial expansion in the form of three cuboids with the length of its sides equal to a,a and b","k=2 for n=3 represents 3ab^2 from thr binomial expansion in the form of three cuboids with the length of its sides equal to a,b and b","k=3 for n=3 represents b^3 from thr binomial expansion in the form of a cube with the length of its side equal to b"]
+
 document.getElementById('button2').style.visibility = 'hidden';
 function next(){
-
     n = document.getElementById("n").value;
     let copyN = n;
     n = Number(n);
@@ -34,7 +39,7 @@ document.getElementById('n').disabled = true;
 document.getElementById('errorId').innerHTML = "";         
 }
 else{
-    document.getElementById('errorId').innerHTML = "n should be an integer between 0 and 3";
+    document.getElementById('errorId').innerHTML = "\[n\] should be an integer between 0 and 3";
 }}
 function Generate()
 {
@@ -52,7 +57,20 @@ function Generate()
     selection.setAttribute("class","selection");
     const image1 = document.getElementById('output');
     image1.src=`images/${n}.jpg`;
+    document.getElementById("n_desc").innerHTML=n_arr[n];
     const image2 = document.getElementById('output2');
+    if(n==0){
+        document.getElementById("k_desc").innerHTML=k0[k];
+    }
+    if(n==1){
+        document.getElementById("k_desc").innerHTML=k1[k];
+    }
+    if(n==2){
+        document.getElementById("k_desc").innerHTML=k2[k];
+    }
+    if(n==3){
+        document.getElementById("k_desc").innerHTML=k3[k];
+    }
     image2.src=`images/${n}${k}.jpg`;
     image1.setAttribute("class","image1");
     image2.setAttribute("class","image2");
